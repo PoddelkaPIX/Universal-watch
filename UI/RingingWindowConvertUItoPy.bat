ad29ce1a0cde6783c7263a2466b92838b563f81c
PoddelkaPIX <69106674+PoddelkaPIX@users.noreply.github.com>
2020-11-14 19:31:58 +0500

    Доделал функционал будильника

---
+++ UI/RingingWindowConvertUItoPy.bat
@@ -0,0 +1,1 @@
+pyuic5 C:\Users\gnebe\Desktop\Python\Universal-watch\UI\RingingWindow.ui -o C:\Users\gnebe\Desktop\Python\Universal-watch\RingingWindow.py
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import datetime
-import time
+import sqlite3
 
 import pygame
 from pygame.locals import *
@@ -9,7 +9,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtCore import QTime, QTimer, QUrl
 from PyQt5.QtWidgets import QMainWindow, QLCDNumber, QPushButton, QWidget, QLineEdit, QStackedWidget, QGridLayout, \
     QComboBox, QFileDialog, QMessageBox, QLabel, QListWidget, QFormLayout, QHBoxLayout, QVBoxLayout, QDialog, \
-    QErrorMessage
+    QErrorMessage, QInputDialog, QListView
 from pygame.mixer import Sound
 
 from AppInterface import Ui_MainWindow
@@ -19,8 +19,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
-        self.FileName = 'C:/Users/gnebe/Desktop/Python/Universal-watch/Sounds/5Sta Family - Первый снег.mp3'
 
         # Подключение к кнопкам функционал
         self.AlarmClockBut.clicked.connect(self.OpenAlarmClock)
@@ -63,11 +61,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.HoursComboBox.addItem(str(i))
 
         self.MusicListComboBox = QComboBox(self.ClockWidget)
-        self.MusicListComboBox.addItem('<Музыка не выбрана>')
+        self.MusicListComboBox.addItem('<Выбрать песню>')
         self.MusicListComboBox.activated.connect(self.UpdateMusic)
+        self.MusicListComboBox.setMaximumWidth(200)
 
         self.ListDeleteAlarmClocks = QListWidget(self.ClockWidget)
-        self.ListDeleteAlarmClocks .setMaximumWidth(20)
+        self.ListDeleteAlarmClocks.setMaximumWidth(20)
         self.ListDeleteAlarmClocks.itemClicked.connect(self.DeleteAlarmClock)
 
         self.ListOfPastAlarmClocks = QListWidget(self.ClockWidget)
@@ -78,7 +77,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.HoursComboBox)
 
         self.verticalLayout = QHBoxLayout()
-        #self.verticalLayout.addWidget(self.ListOfAlarmClocks)
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.addWidget(self.AddMusicButton)
@@ -89,7 +87,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.gridLayout = QGridLayout(self.ClockWidget)
         self.gridLayout.addWidget(self.AddAlarmButton, 4, 1, 1, 1)
         self.gridLayout.addWidget(self.ListOfPastAlarmClocks, 2, 1, 1, 1)
-        self.gridLayout.addWidget(self.ListDeleteAlarmClocks , 2, 2, 1, 1)
+        self.gridLayout.addWidget(self.ListDeleteAlarmClocks, 2, 2, 1, 1)
         self.gridLayout.addLayout(self.formLayout, 6, 1, 1, 1)
         self.gridLayout.addLayout(self.horizontalLayout, 4, 2, 1, 1)
         self.gridLayout.addLayout(self.verticalLayout, 2, 2, 1, 1)
@@ -97,15 +95,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.gridLayout.addItem(SpacerItem, 2, 1, 1, 1)
 
         # Интерфейс секундомера
-        self.formLayout = QtWidgets.QFormLayout(self.StopWatchWidget)
+        self.lcdNumber = QLCDNumber(self.StopWatchWidget)
+
+        self.StartPushButton = QPushButton('Старт', self.StopWatchWidget)
+
+        self.StopPushButton = QPushButton('Стоп', self.StopWatchWidget)
+
+        self.listView = QListView(self.StopWatchWidget)
+        self.listView.setMinimumHeight(400)
+
         self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.lcdNumber = QtWidgets.QLCDNumber(self.StopWatchWidget)
         self.verticalLayout.addWidget(self.lcdNumber)
-        self.formLayout.setLayout(0, QtWidgets.QFormLayout.FieldRole, self.verticalLayout)
-        self.pushButton = QtWidgets.QPushButton(self.StopWatchWidget)
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.pushButton)
-        self.listView = QtWidgets.QListView(self.StopWatchWidget)
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.listView)
+
+        self.formLayout = QFormLayout(self.StopWatchWidget)
+        self.formLayout.setLayout(0, QFormLayout.FieldRole, self.verticalLayout)
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.StartPushButton)
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.StopPushButton)
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.listView)
 
         # Добавление виджетов с устройствами в главное размещение
         self.MainLayout.addWidget(self.ClockWidget)
@@ -116,8 +122,41 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Запуск таймера
         self.showDateTime()
+        self.LoadAlarmClocks()
 
-        # 'C:/Users/gnebe/Desktop/Python/Universal-watch/Sounds/5Sta Family - Первый снег.mp3'
+        self.wid = QWidget()
+        self.wid.resize(400, 300)
+        self.verticalLayout = QVBoxLayout(self.wid)
+        self.label = QLabel(self.wid)
+        self.verticalLayout.addWidget(self.label)
+        self.horizontalLayout = QHBoxLayout()
+        self.Ok_button = QPushButton('OK', self.wid)
+        self.Ok_button.clicked.connect(self.OkPressed)
+        self.horizontalLayout.addWidget(self.Ok_button)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        with open('AddressMusic.txt', 'r') as Address:
+            add = Address.readline()
+            self.AddressMusicLine.setText(add)
+
+            for i in os.listdir(add):
+                if '.mp3' in i:
+                    self.MusicListComboBox.addItem(i)
+
+            self.AddressMusic = add
+
+        with open('SelectedMusic.txt', 'r') as Music:
+            self.MusicListComboBox.currentIndex()
+            self.FileName = self.AddressMusic + '/' + Music.read()
+
+    def LoadAlarmClocks(self):
+        con = sqlite3.connect('AlarmClock.db')
+        cur = con.cursor()
+        result = cur.execute("""SELECT Time FROM Clocks""").fetchall()
+        for elem in result:
+            self.ListOfPastAlarmClocks.addItem(''.join(elem))
+            self.ListDeleteAlarmClocks.addItem('X')
+        con.close()
 
     def showDateTime(self):
         timeNow = QTime.currentTime()
@@ -131,58 +170,108 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def StartORStopAlarmClock(self):
         a = self.ListOfPastAlarmClocks.currentItem().text().replace('<<(', '')
         a = a.replace(')>>', '')
+        con = sqlite3.connect('AlarmClock.db')
+        cur = con.cursor()
         if '<<(' in self.ListOfPastAlarmClocks.currentItem().text():
             self.ListOfPastAlarmClocks.currentItem().setText(a)
+            cur.execute("""UPDATE Clocks set Time = ? WHERE Time = ?""", (a, '<<(' + a + ')>>'))
         else:
             self.ListOfPastAlarmClocks.currentItem().setText('<<(' + a + ')>>')
+            cur.execute("""UPDATE Clocks set Time = ? WHERE Time = ?""", ('<<(' + a + ')>>', a))
+        con.commit()
+        con.close()
 
     def DeleteAlarmClock(self):
         Row = self.ListDeleteAlarmClocks.currentIndex().row()
+        Item = self.ListOfPastAlarmClocks.item(Row).text()
         self.ListOfPastAlarmClocks.takeItem(Row)
         self.ListDeleteAlarmClocks.takeItem(Row)
+
+        con = sqlite3.connect('AlarmClock.db')
+        cur = con.cursor()
+        cur.execute("""DELETE FROM Clocks WHERE Time = ?""", (Item,)).fetchall()
+        con.commit()
+        con.close()
 
     def AlarmIsRinging(self):
         time = QTime.currentTime()
         text = time.toString('hh:mm')
-        a = '<<(' + text[:2] + ':' + text[3:] + ')>>'
+        ThisTime = '<<(' + text[:2] + ':' + text[3:] + ')>>'
         for index in range(self.ListOfPastAlarmClocks.count()):
-            if a == self.ListOfPastAlarmClocks.item(index).text():
+            if ThisTime == self.ListOfPastAlarmClocks.item(index).text():
                 b = self.ListOfPastAlarmClocks.item(index).text().replace('<<(', '')
                 b = b.replace(')>>', '')
+
+                con = sqlite3.connect('AlarmClock.db')
+                cur = con.cursor()
+                cur.execute("""UPDATE Clocks SET Time = ? WHERE Time = ?""", (b, ThisTime)).fetchall()
+                con.commit()
+                con.close()
+
                 self.ListOfPastAlarmClocks.item(index).setText(b)
+
                 pygame.mixer.init()
                 pygame.mixer.music.load(self.FileName)
                 pygame.mixer.music.play()
 
-                msg = QMessageBox()
-                msg.setText(a)
-                msg.setIcon(QMessageBox.Warning)
-                msg.setInformativeText('Будильник>')
-                msg.setWindowTitle("Будильник")
-                msg.exec_()
-                pygame.mixer.music.stop()
+                path = 'ClockRiding.gif'
+                gif = QtGui.QMovie(path)
+                self.label.setMovie(gif)
+                gif.start()
+
+                self.wid.show()
+
+    def OkPressed(self):
+        pygame.mixer.music.stop()
+        self.wid.hide()
 
     def UpdateMusic(self):
         self.FileName = self.AddressMusicLine.text() + '/' + self.MusicListComboBox.currentText()
+        with open('SelectedMusic.txt', 'w') as Music:
+            Music.write(self.MusicListComboBox.currentText())
 
     def DialogAddMusic(self):
         try:
             fname = QFileDialog.getExistingDirectory(self, "Выбрать папку", ".")
             files = os.listdir(fname)
+            self.MusicListComboBox.clear()
             for i in files:
                 if '.mp3' in i:
                     self.MusicListComboBox.addItem(i)
             self.AddressMusicLine.setText(fname)
             self.MusicListComboBox.setCurrentIndex(1)
+
+            with open('AddressMusic.txt', 'w') as file:
+                file.write(fname)
+
+
+            # con = sqlite3.connect('AlarmClock.db')
+            # cur = con.cursor()
+            # cur.execute("""UPDATE Clocks set""", (item, )).fetchall()
+            # con.commit()
+            # con.close()
         except:
-            print('Что-то не так с музыкой')
+            print('Музыка не была выбрана')
 
     def AddAlarmsComboBoxItem(self):
+        Alarm = False
         if self.ListOfPastAlarmClocks.count() < 14:
             item = self.HoursComboBox.currentText() + ':' + self.MinutesComboBox.currentText()
-            if '<<(' + item + ')>>' not in self.ListOfPastAlarmClocks.selectedItems():
+            for i in range(self.ListOfPastAlarmClocks.count()):
+                if item in self.ListOfPastAlarmClocks.item(i).text():
+                    Alarm = True
+            if Alarm == False:
                 self.ListOfPastAlarmClocks.addItem('<<(' + item + ')>>')
+
+                item = '<<(' + item + ')>>'
+                con = sqlite3.connect('AlarmClock.db')
+                cur = con.cursor()
+                cur.execute("""INSERT INTO Clocks (Time) values(?)""", (item,)).fetchall()
+                con.commit()
+                con.close()
+
                 self.ListDeleteAlarmClocks.addItem('X')
+                Alarm = False
         else:
             msg = QMessageBox()
             msg.setText("Слишком много будильников.")
